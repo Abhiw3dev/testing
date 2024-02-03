3bdb51acfe6b394d3eb80e217519469fb8f02207
Abhishek W3Dev <abhishek.k@w3dev.email>
2024-02-03 11:52:23 +0530

    testphase3

--- __test__/arbsearch.test.js
+++ __test__/arbsearch.test.js
@@ -1,25 +1,23 @@
-const searchTime = require('../src/arbSearch')
-
-
-
+const searchTime = require('../src/index.js')
+ 
 describe('Search Time Measurement', () => {
-  it('should measure search time for appreviewbot.com', async () => {
+  it('should measure search time for appreviewbot.com', () => {
     const targetURL = 'https://appreviewbot.com/';
     const searchTerm = 'Facebook';
-    const responseTime = await  searchTime(targetURL, searchTerm);
+    const responseTime = searchTime(targetURL, searchTerm);
     
     console.log(`Response time for appreviewbot.com: ${responseTime} seconds`);
     
     // Add your Jest assertions here, e.g., expect(responseTime).toBeLessThan(10);
-  },15000);
+  },60000);
 
-//   it('should measure search time for next.appreviewbot.com', async () => {
-//     const targetURL = 'https://next.appreviewbot.com/';
-//     const searchTerm = 'Slack';
-//     const responseTime = await searchTime(targetURL, searchTerm);
+  it('should measure search time for next.appreviewbot.com', async () => {
+    const targetURL = 'https://next.appreviewbot.com/';
+    const searchTerm = 'Slack';
+    const responseTime = await searchTime(targetURL, searchTerm);
     
-//     console.log(`Response time for next.appreviewbot.com: ${responseTime} seconds`);
+    console.log(`Response time for next.appreviewbot.com: ${responseTime} seconds`);
     
-//     // Add your Jest assertions here, e.g., expect(responseTime).toBeLessThan(10);
-//   });
+    // Add your Jest assertions here, e.g., expect(responseTime).toBeLessThan(10);
+  },60000);
 });
